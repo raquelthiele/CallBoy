ba31c0c06ea4e76d3377ef2f5d68b83ce40b8c1c
Raquel Thiele <raquelthiele@hotmail.com>
2016-05-21 17:21:51 -0300

    Criação do modelo e suas classes.

---
+++ app/src/main/java/br/com/ramada/callboy/model/Agenda.java
@@ -0,0 +1,8 @@
+package br.com.ramada.callboy.model;
+
+/**
+ * Created by Ramada on 21/05/2016.
+ */
+public class Agenda {
+
+}
---
+++ app/src/main/java/br/com/ramada/callboy/model/Contato.java
@@ -0,0 +1,7 @@
+package br.com.ramada.callboy.model;
+
+/**
+ * Created by Ramada on 21/05/2016.
+ */
+public class Contato {
+}
