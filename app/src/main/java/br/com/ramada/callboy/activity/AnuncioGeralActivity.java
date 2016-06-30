8c8f80380a68bc3ebf8669c3e0c875adeebe474b
marinaSartori <marina.sartori@uniriotec.br>
2016-06-29 23:00:15 -0300

    tela de anúncio e leitura de sms criadas

---
+++ app/src/main/java/br/com/ramada/callboy/activity/AnuncioGeralActivity.java
@@ -0,0 +1,15 @@
+package br.com.ramada.callboy.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.ramada.callboy.R;
+
+public class AnuncioGeralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anunciar_geral);
+    }
+}
